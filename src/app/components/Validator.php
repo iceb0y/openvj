ac5666587f6efb4d859491451a8bd2f64e33b087
Breezewish <me@breeswish.org>
2013-08-08 22:18:56 +0800

    \VJ\Validator support mongoid and array validating

--- src/app/components/Validator.php
+++ src/app/components/Validator.php
@@ -18,11 +18,81 @@ class Validator
 
         foreach ($params as $param) {
             if (!isset($checkArray[$param])) {
-                return \VJ\I::error('MISSING_ARGUMENT', $param);
+                return \VJ\I::error('ARGUMENT_MISSING', $param);
             }
         }
 
         return true;
+    }
+
+    /**
+     * 检查并返回MongoId对象，若失败则返回null
+     *
+     * @param $p
+     *
+     * @return \MongoId|null
+     */
+    public static function mongoId($p)
+    {
+
+        $p = (string)$p;
+
+        if (strlen($p) !== 24) {
+            return null;
+        }
+
+        try {
+
+            $p = new \MongoId($p);
+            return $p;
+
+        } catch(\MongoException $e) {
+
+            return null;
+
+        }
+
+    }
+
+    /**
+     * 根据指定的规则过滤数组
+     *
+     * @param $data
+     * @param $rules
+     *
+     * @return array
+     */
+    public static function filter($data, $rules)
+    {
+
+        $ret = array();
+
+        foreach ($rules as $key => $rule)
+        {
+
+            switch($rule)
+            {
+                case 'int':
+                    $ret[$key] = (int)$data[$key];
+                    break;
+
+                case 'string':
+                    $ret[$key] = (string)$data[$key];
+                    break;
+
+                case 'html':
+                    $ret[$key] = \VJ\Escaper::html($data[$key]);
+                    break;
+
+                case null:
+                    $ret[$key] = $data[$key];
+                    break;
+            }
+
+        }
+
+        return $ret;
+
     }
 
 }
--- src/app/includes/errorcode.php
+++ src/app/includes/errorcode.php
@@ -1,3 +1,8 @@
 <?php
 
-const ERR_MISSING_ARGUMENT = 'Missing argument: %s';
+const ERR_ARGUMENT_MISSING = 'Missing argument: %s';
+const ERR_ARGUMENT_REQUIRED = '%s is required';
+const ERR_ARGUMENT_INVALID = 'Invalid argument: %s';
+const ERR_NO_PRIV = 'You do not have the privilege (%s) to perform this action';
+const ERR_USER_NOTFOUND = 'User not found';
+const ERR_FAILED = 'Operation failed';
