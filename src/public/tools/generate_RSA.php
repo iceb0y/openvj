8fc06757f0f12757e771935e17feefc74f3bf239
Breezewish <me@breeswish.org>
2013-08-09 17:45:40 +0800

    add rsa generation tools

---
+++ src/public/tools/generate_RSA.php
@@ -0,0 +1,19 @@
+<?php
+
+set_include_path(__DIR__.'/../../app/vendor/phpseclib/phpseclib/phpseclib/');
+require_once('Crypt/RSA.php');
+
+header('content-type:text/plain');
+
+define('CRYPT_RSA_MODE', CRYPT_RSA_MODE_INTERNAL);
+$rsa = new Crypt_RSA();
+$rsa->setPublicKeyFormat(CRYPT_RSA_PUBLIC_FORMAT_RAW);
+$key = $rsa->createKey(1024);
+echo $key['privatekey'];
+echo "\n\n";
+$e = new Math_BigInteger($key['publickey']['e'], 10);
+$n = new Math_BigInteger($key['publickey']['n'], 10);
+echo "Public Key:\n";
+echo $e->toHex();
+echo "\n";
+echo $n->toHex();
