ca3d1e98f0d2c7f6d9a1aa807be394d61b1dc83f
66CCFF <oxalca@gmail.com>
2013-08-10 08:20:52 +0800

    add en_US i18n js file

---
+++ src/public/static/i18n/en_US.js
@@ -0,0 +1,14 @@
+(function(I18N)
+{
+
+    I18N.register('en_US', {
+
+        'form.login.title':     'Login to Vijos', 
+        'form.login.l_user':    'Username',
+        'form.login.l_pass':    'password',
+        'form.login.b_login':   'Login',
+        'form.login.b_cancel':  'Cancel'
+        
+    });
+
+})(VJ.I18N);
