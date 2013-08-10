8a1e9e01276a072a02cff31e77324ab146666791
Breezewish <me@breeswish.org>
2013-08-10 16:50:52 +0800

    node::io bug fix

--- src/app/components/Node.php
+++ src/app/components/Node.php
@@ -4,37 +4,38 @@ namespace VJ;
 
 class Node
 {
-	public static function io($location, $get = null, $post = null, $getResponse = false)
-	{
-		global $config;
-		//处理请求数据
-		$url = ENV_NODE_HOST_IO.'/'.$location;
+    public static function io($location, $get = null, $post = null, $getResponse = false)
+    {
+        global $config;
 
-		if ($get != null && is_array($get)) {
-			$url = $url.'?'.http_build_query($get, '', '&');
-		}
+        //处理请求数据
+        $url = $config->Nodejs->host.$location;
 
-		//初始化
-		$curl = curl_init($url);
-		curl_setopt_array($curl, array
-		(
-			CURLOPT_CONNECTTIMEOUT => 2,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_PORT           => ENV_NODE_PORT_IO,
-			CURLOPT_HEADER         => false,
-			CURLOPT_NOBODY         => !$getResponse
-		));
+        if ($get != null && is_array($get)) {
+            $url = $url.'?'.http_build_query($get, '', '&');
+        }
 
-		//处理POST
-		if ($post != null && is_array($post)) {
-			curl_setopt($curl, CURLOPT_HTTPHEADER, array('Expect:'));
-			curl_setopt($curl, CURLOPT_POST, true);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array('json' => json_encode($post)), '', '&'));
-		}
+        //初始化
+        $curl = curl_init($url);
+        curl_setopt_array($curl, array
+        (
+            CURLOPT_CONNECTTIMEOUT => 2,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_PORT           => $config->Nodejs->port,
+            CURLOPT_HEADER         => false,
+            CURLOPT_NOBODY         => !$getResponse
+        ));
 
-		$data = curl_exec($curl);
-		curl_close($curl);
+        //处理POST
+        if ($post != null && is_array($post)) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Expect:'));
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array('json' => json_encode($post)), '', '&'));
+        }
 
-		return $data;
-	}
+        $data = curl_exec($curl);
+        curl_close($curl);
+
+        return $data;
+    }
 }
--- src/app/configs/project.php
+++ src/app/configs/project.php
@@ -4,8 +4,7 @@ define('ENV_REQUEST_TIME', isset($_SERVER['REQUEST_TIME_FLOAT']) ? $_SERVER['REQ
 define('ENV_SSL', (bool)(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'));
 define('ENV_HOST', $_SERVER['HTTP_HOST']);
 define('ENV_HOST_URL', (ENV_SSL ? 'https' : 'http').'://'.ENV_HOST);
-define('ENV_NODE_HOST_IO', ENV_HOST_URL.'/node');
-define('ENV_NODE_PORT_IO', '1234');
+
 define('ROOT_DIR', __dir__.'/../../');
 define('APP_DIR', ROOT_DIR.'app/');
 
