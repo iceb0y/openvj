8f34736ff7f418ac524fe5a089f349d844c71dd7
Breezewish <me@breeswish.org>
2013-08-11 16:33:54 +0800

    fix

--- src/public/index.php
+++ src/public/index.php
@@ -14,8 +14,6 @@ require __DIR__.'/../app/includes/init.php';
 \VJ\User\Security\Privilege::initialize();
 \VJ\User\Account::initialize();
 
-\VJ\Node::io($_SERVER['HTTP_HOST']);
-
 //============================================
 
 $app = new \Phalcon\Mvc\Application(\Phalcon\DI::getDefault());
