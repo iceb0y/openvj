2b6ab119cb629a74d8e01ef81c11f29c25907aa0
Breezewish <me@breeswish.org>
2013-08-21 17:24:30 +0800

    remove apc dependency for privilege

--- src/app/includes/privilege.php
+++ src/app/includes/privilege.php
@@ -1,55 +1,49 @@
 <?php
 
-if (!apc_load_constants('openvj-const-privilege')) {
-    apc_define_constants('openvj-const-privilege', [
+const PRIV = 0; //总权限
 
-        'PRIV'                          => 0, //总权限
+//开发
+const PRIV_DEV = 10; //Development
+const PRIV_DEV_ACCESS = 11; //访问DevelopmentHome
+const PRIV_DEV_DB = 20; //修改数据库
+const PRIV_DEV_EXEC = 40; //执行任意指令
 
-        //开发
-        'PRIV_DEV'                      => 10, //Development
-        'PRIV_DEV_ACCESS'               => 11, //访问DevelopmentHome
-        'PRIV_DEV_DB'                   => 20, //修改数据库
-        'PRIV_DEV_EXEC'                 => 40, //执行任意指令
+//访问
+const PRIV_LOG_IN = 1000; //登录到该账户
+const PRIV_LOG_OUT = 1001; //登出
 
-        //访问
-        'PRIV_LOG_IN'                   => 1000, //登录到该账户
-        'PRIV_LOG_OUT'                  => 1001, //登出
+//用户
+const PRIV_USER = 10000; //用户总权限
+const PRIV_USER_BAR = 10001; //用户控制条
 
-        //用户
-        'PRIV_USER'                     => 10000, //用户总权限
-        'PRIV_USER_BAR'                 => 10001, //用户控制条
+//题目
+const PRIV_PROBLEM = 11000; //题库总权限
 
-        //题目
-        'PRIV_PROBLEM'                  => 11000, //题库总权限
+//题单
+const PRIV_PROBLEMLIST = 12000; //题单总权限
 
-        //题单
-        'PRIV_PROBLEMLIST'              => 12000, //题单总权限
+//评测记录
+const PRIV_RECORD = 13000; //记录总权限
 
-        //评测记录
-        'PRIV_RECORD'                   => 13000, //记录总权限
+//讨论
+const PRIV_DISCUSSION = 14000; //讨论总权限
+const PRIV_DISCUSSION_REPLY_TOPIC = 14101; //创建评论
+const PRIV_DISCUSSION_REPLY_COMMENT = 14102; //回复评论
 
-        //讨论
-        'PRIV_DISCUSSION'               => 14000, //讨论总权限
-        'PRIV_DISCUSSION_REPLY_TOPIC'   => 14101, //创建评论
-        'PRIV_DISCUSSION_REPLY_COMMENT' => 14102, //回复评论
+//比赛
+const PRIV_TEST = 15000; //比赛总权限
 
-        //比赛
-        'PRIV_TEST'                     => 15000, //比赛总权限
+//团队
+const PRIV_TEAM = 16000; //团队总权限
 
-        //团队
-        'PRIV_TEAM'                     => 16000, //团队总权限
+//RP
+const PRIV_RP = 17000; //RP总权限
 
-        //RP
-        'PRIV_RP'                       => 17000, //RP总权限
+//App
+const PRIV_APP = 18000; //应用总权限
 
-        //App
-        'PRIV_APP'                      => 18000, //应用总权限
+//其他
+const PRIV_ETC = 19000; //其他总权限
 
-        //其他
-        'PRIV_ETC'                      => 19000, //其他总权限
-
-        //管理
-        'PRIV_ADMIN'                    => 20000, //进入管理后台
-
-    ]);
-}
+//管理
+const PRIV_ADMIN = 20000; //进入管理后台
