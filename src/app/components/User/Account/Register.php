f7b561fdfcc92d8b3dc5acce102dbc0fcab8d90e
Breezewish <me@breeswish.org>
2013-08-26 21:51:58 +0800

    use a more beautiful way to store empty object into mongo

--- src/app/components/User/Account/Register.php
+++ src/app/components/User/Account/Register.php
@@ -259,10 +259,10 @@ class Register
         $user->sig      = '';
         $user->sigm     = '';
         $user->group    = GROUP_USER;
-        $user->acl      = serialize(['_' => null]); //avoid unnecessary unserialization
-        $user->aclrule  = serialize(['_' => null]);
-        $user->privacy  = ['_' => null];
-        $user->stars    = ['_' => null];
+        $user->acl      = serialize([]); //avoid unnecessary unserialization
+        $user->aclrule  = serialize([]);
+        $user->privacy  = new \stdClass();
+        $user->stars    = new \stdClass();
         $user->pbms     = [
             'pass'    => 0,
             'passlst' => [],
