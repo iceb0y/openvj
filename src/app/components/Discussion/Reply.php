c4e85cfdbc4cf94479aea1b0bfee299dea86fbfa
Breezewish <me@breeswish.org>
2013-08-28 20:54:50 +0800

    implement reply topic

--- src/app/components/Discussion/Reply.php
+++ src/app/components/Discussion/Reply.php
@@ -18,9 +18,15 @@ class Reply
      */
     public static function topic($topic_id, $content)
     {
-        global $__CONFIG;
 
-        $acl = \Phalcon\DI::getDefault()->getShared('acl');
+        $di = \Phalcon\DI::getDefault();
+        $acl = $di->getShared('acl');
+        $mongo = $di->getShared('mongo');
+
+        $topic_id = (string)$topic_id;
+        $content = (string)$content;
+
+        global $__CONFIG, $_UID;
 
         if (strlen($topic_id) > 50) {
             return I::error('ARGUMENT_TOO_LONG', 'topic_id', 50);
@@ -38,30 +44,44 @@ class Reply
             return I::error('CONTENT_TOOLONG', $__CONFIG->Discussion->contentMax);
         }
 
+        $document = self::createReplyDocument($content);
+
+        $update = [
+            'luser' => $_UID,
+            'ltime' => time(),
+            'count' => ['$inc' => 1],
+            'r'     => ['$push' => $document]
+        ];
+
+        $mongo->Discussion->update(['_id' => $topic_id], $update, ['upsert' => true]);
+
+        return $document['_id'];
 
     }
 
     /**
-     * 回复评论
+     * 创建标准化回复文档
      *
-     * @param $topic_id
-     * @param $comment_id
-     * @param $content
+     * @param $markdownContent
      *
      * @return array
      */
-    public static function comment($topic_id, $comment_id, $content)
+    private static function createReplyDocument($markdownContent)
     {
-        
-        $acl = \Phalcon\DI::getDefault()->getShared('acl');
 
-        if (strlen($topic_id) > 50) {
-            return I::error('ARGUMENT_TOO_LONG', 'topic_id', 50);
-        }
+        global $_UID;
 
-        if (!$acl->has(PRIV_DISCUSSION_REPLY_COMMENT)) {
-            return I::error('NO_PRIV', 'PRIV_DISCUSSION_REPLY_COMMENT');
-        }
+        return [
+
+            '_id'  => uniqid(),
+            'uid'  => $_UID,
+            'time' => time(),
+            'md'   => $markdownContent,
+            'text' => \VJ\Formatter\Markdown::parse($markdownContent),
+            'vote' => ['sp' => new \stdClass(), 'ob' => new \stdClass()],
+            'xtra' => new \stdClass(),
+
+        ];
 
     }
 
