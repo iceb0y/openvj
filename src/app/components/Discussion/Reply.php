40a78ed0b8a4a9b4a73cbf889fa388025944290a
Breezewish <me@breeswish.org>
2013-08-28 21:08:58 +0800

    implement reply_comment

--- src/app/components/Discussion/Reply.php
+++ src/app/components/Discussion/Reply.php
@@ -45,6 +45,7 @@ class Reply
         }
 
         $document = self::createReplyDocument($content);
+        $document['r'] = [];
 
         $update = [
             'luser' => $_UID,
@@ -54,6 +55,57 @@ class Reply
         ];
 
         $mongo->Discussion->update(['_id' => $topic_id], $update, ['upsert' => true]);
+
+        return $document['_id'];
+
+    }
+
+    /**
+     * 回复评论
+     *
+     * @param $topic_id
+     * @param $comment_id
+     * @param $content
+     *
+     * @return array
+     */
+    public static function comment($topic_id, $comment_id, $content)
+    {
+
+        $di = \Phalcon\DI::getDefault();
+        $acl = $di->getShared('acl');
+        $mongo = $di->getShared('mongo');
+
+        $topic_id = (string)$topic_id;
+        $comment_id = (string)$comment_id;
+        $content = (string)$content;
+
+        global $__CONFIG, $_UID;
+
+        if (strlen($topic_id) > 50) {
+            return I::error('ARGUMENT_TOO_LONG', 'topic_id', 50);
+        }
+
+        if (!$acl->has(PRIV_DISCUSSION_REPLY_COMMENT)) {
+            return I::error('NO_PRIV', 'PRIV_DISCUSSION_REPLY_COMMENT');
+        }
+
+        $document = self::createReplyDocument($content);
+
+        $result = $mongo->Discussion->update(
+            [
+                '_id' => $topic_id,
+                'r._id' => $comment_id
+            ],
+            [
+                '$push' => ['r.$.r' => $document]
+            ]
+        );
+
+        if ($result['n'] == 0) {
+            //no document found
+            return I::error('NOT_FOUND', 'discussion or topic');
+        }
 
         return $document['_id'];
 
