a4194465ecb46fcd774d560fe30443b7343e8bd6
Breezewish <me@breeswish.org>
2013-08-30 12:41:07 +0800

    cli generate_RSA

--- tools/generate_RSA.php
+++ tools/generate_RSA.php
@@ -6,12 +6,19 @@ require_once('Crypt/RSA.php');
 define('CRYPT_RSA_MODE', CRYPT_RSA_MODE_INTERNAL);
 $rsa = new Crypt_RSA();
 $rsa->setPublicKeyFormat(CRYPT_RSA_PUBLIC_FORMAT_RAW);
+
+// Private key
+
 $key = $rsa->createKey(1024);
+echo 'Private key:',PHP_EOL,PHP_EOL;
 echo $key['privatekey'];
-echo "\n\n";
+
+// Public key
+
 $e = new Math_BigInteger($key['publickey']['e'], 10);
 $n = new Math_BigInteger($key['publickey']['n'], 10);
-echo "Public Key:\n";
-echo $e->toHex();
-echo "\n";
-echo $n->toHex();
+echo PHP_EOL,PHP_EOL,'Public key:',PHP_EOL,PHP_EOL;
+echo 'e = ', $e->toHex();
+echo PHP_EOL;
+echo 'n = ', $n->toHex();
+echo PHP_EOL;
