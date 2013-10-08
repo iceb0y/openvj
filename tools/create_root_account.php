7e588198e97ea7b70f852a881914d272de2c0550
Breezewish <me@breeswish.org>
2013-10-08 15:02:24 +0800

    support override 'root' in create_root_account

--- tools/create_root_account.php
+++ tools/create_root_account.php
@@ -1,6 +1,27 @@
 <?php
 
+define('OPENVJ_ACL_DISABLE', true);
+
 require __DIR__.'/../src/app/includes/init.php';
+
+$uid = null;
+$existance = \VJ\User\Account::usernameExists('root', $uid);
+
+if (!in_array('--force', $argv)) {
+
+    if ($existance) {
+        echo "User \"root\" is already existed (uid=$uid), operation aborted.\nUse --force to override.\n";
+        exit();
+    }
+
+} else {
+
+    if ($existance) {
+        \VJ\User\Account::delete($uid, true);
+        echo "Warning: User \"root\" has been overridden (uid=$uid).\n";
+    }
+
+}
 
 $result = \VJ\User\Account\Register::register('root', 'openvjroot', 'root', 1, 'accept', [
 
