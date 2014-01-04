5a7e9dc67f5e3c38c2b0f4b4e64603ef05db94d5
Breezewish <me@breeswish.org>
2014-01-04 20:37:26 +0800

    implement new randomize method

--- src/app/components/Security/Randomizer.php
+++ src/app/components/Security/Randomizer.php
@@ -5,14 +5,24 @@ namespace VJ\Security;
 class Randomizer
 {
 
+    public static function toBinary($bytes = 10)
+    {
+        return openssl_random_pseudo_bytes($bytes);
+    }
+
     public static function toHex($bytes = 10)
     {
-        return bin2hex(openssl_random_pseudo_bytes($bytes));
+        return bin2hex(self::toBinary($bytes));
+    }
+
+    public static function toBase64($bytes = 10)
+    {
+        return base64_encode(self::toBinary($bytes));
     }
 
     public static function toSHA1($bytes = 10)
     {
-        return sha1(self::toHex($bytes));
+        return sha1(self::toBinary($bytes));
     }
 
 }
