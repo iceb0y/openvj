92311eae37d9fc5dded71ec811d8bcb5f1c88c91
Breezewish <me@breeswish.org>
2014-07-16 22:17:54 +0800

    added toAjaxObject interface

--- src/app/components/Exception.php
+++ src/app/components/Exception.php
@@ -4,11 +4,25 @@ namespace VJ;
 
 class Exception extends \Exception
 {
-    const CODE_LOGIC_ERROR = 0;
+    const CODE_USER_ERROR = 100;
 
     public function __construct()
     {
         $message = call_user_func_array('\VJ\I18N::get', func_get_args());
-        parent::__construct($message, self::CODE_LOGIC_ERROR);
+        parent::__construct($message, self::CODE_USER_ERROR);
+    }
+
+    public function toAjaxObject()
+    {
+        global $__CONFIG;
+
+        $obj = ['succeeded' => false, 'message' => $this->message];
+        if ($__CONFIG->Debug->enabled) {
+            $obj['file'] = $this->file;
+            $obj['line'] = $this->line;
+            $obj['trace'] = $this->getTraceAsString();
+        }
+
+        return $obj;
     }
 }
